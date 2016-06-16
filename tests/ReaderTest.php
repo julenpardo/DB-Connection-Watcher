79aad4ce9b861f3df43d08eb8c40a56ac9c6dc3d
Julen Pardo <julen.pardo@alumni.mondragon.edu>
2016-06-16 11:35:03 +0200

    dev-config: code tests for "checkConfiguration"

--- tests/ReaderTest.php
+++ tests/ReaderTest.php
@@ -12,7 +12,13 @@ use DBConnectionWatcher\Configuration\Reader;
 
 class ReaderTest extends PHPUnit_Framework_TestCase
 {
+    protected $reader;
     protected $configurationFile = 'configurationTest.ini';
+
+    protected function setUp()
+    {
+        $this->reader = new Reader();
+    }
 
     protected function tearDown()
     {
@@ -24,6 +30,15 @@ class ReaderTest extends PHPUnit_Framework_TestCase
         if (file_exists($this->configurationFile)) {
             unlink($this->configurationFile);
         }
+    }
+
+    protected static function get_method($name)
+    {
+        $class = new ReflectionClass('DBConnectionWatcher\Configuration\Reader');
+        $method = $class->getMethod($name);
+        $method->setAccessible(true);
+
+        return $method;
     }
 
     /**
@@ -69,6 +84,129 @@ class ReaderTest extends PHPUnit_Framework_TestCase
             $actual = Reader::readConfiguration($this->configurationFile);
 
             $this->assertEquals($expected, $actual);
+        } catch (Exception $exception) {
+            $this->fail("No exception should be thrown.");
+        }
+    }
+
+    /**
+     * @expectedException Exception
+     */
+    public function testCheckConfigurationFalse()
+    {
+        $method = self::get_method('checkConfiguration');
+
+        $method->invokeArgs($this->reader, [false]);
+    }
+
+    /**
+     * @expectedException \DBConnectionWatcher\Configuration\InvalidConfigurationFormatException
+     */
+    public function testCheckConfigurationInvalidConfigurationFormatException()
+    {
+        $method = self::get_method('checkConfiguration');
+
+        $configuration = ['section 1' => 'not an array!'];
+
+        $method->invokeArgs($this->reader, [$configuration]);
+    }
+
+    /**
+     * @expectedException \DBConnectionWatcher\Configuration\MissingOrExtraConfigurationsException
+     */
+    public function testCheckConfigurationMissingOrExtraConfigurationsException()
+    {
+        $method = self::get_method('checkConfiguration');
+
+        $configuration = [
+            'section 1' => [
+                'database' => 'testdb',
+            ]
+        ];
+
+        $method->invokeArgs($this->reader, [$configuration]);
+    }
+
+    /**
+     * @expectedException \DBConnectionWatcher\Configuration\InvalidConfigurationPropertyException
+     */
+    public function testCheckConfigurationInvalidConfigurationPropertyException()
+    {
+        $method = self::get_method('checkConfiguration');
+
+        $configuration = [
+            'section 1' => [
+                'database' => 'testdb',
+                'username' => 'postgres',
+                'password' => 'postgres',
+                'host'     => 'localhost',
+                'port'     => '5433',
+                'typo error' => 'julen.pardo@outlook.es'
+            ]
+        ];
+
+        $method->invokeArgs($this->reader, [$configuration]);
+    }
+
+    /**
+     * @expectedException \DBConnectionWatcher\Configuration\InvalidConfigurationValueException
+     */
+    public function testCheckConfigurationInvalidConfigurationValueException()
+    {
+        $method = self::get_method('checkConfiguration');
+
+        $configuration = [
+            'section 1' => [
+                'database' => '',
+                'username' => 'postgres',
+                'password' => 'postgres',
+                'host'     => 'localhost',
+                'port'     => '5433',
+                'typo error' => 'julen.pardo@outlook.es'
+            ]
+        ];
+
+        $method->invokeArgs($this->reader, [$configuration]);
+    }
+
+    /**
+     * @expectedException \DBConnectionWatcher\Configuration\InvalidConfigurationValueTypeException
+     */
+    public function testCheckConfigurationInvalidConfigurationValueTypeException()
+    {
+        $method = self::get_method('checkConfiguration');
+
+        $configuration = [
+            'section 1' => [
+                'database' => 'testdb',
+                'username' => 'postgres',
+                'password' => 'postgres',
+                'host'     => 'localhost',
+                'port'     => 'not a number',
+                'typo error' => 'julen.pardo@outlook.es'
+            ]
+        ];
+
+        $method->invokeArgs($this->reader, [$configuration]);
+    }
+
+    public function testCheckConfiguration()
+    {
+        $method = self::get_method('checkConfiguration');
+
+        $configuration = [
+            'section 1' => [
+                'database' => 'testdb',
+                'username' => 'postgres',
+                'password' => 'postgres',
+                'host'     => 'localhost',
+                'port'     => '5432',
+                'typo error' => 'julen.pardo@outlook.es'
+            ]
+        ];
+
+        try {
+            $method->invokeArgs($this->reader, [$configuration]);
         } catch (Exception $exception) {
             $this->fail("No exception should be thrown.");
         }
