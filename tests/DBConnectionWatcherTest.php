5973963819fab50814b536bee021d14b8b51eee3
Julen Pardo <julen.pardo@alumni.mondragon.edu>
2016-06-17 16:40:36 +0200

    dev-main: code remaining tests for "checkStatus"
    The test that checks that the mail has been sent correctly is skipped, because no way has been found to mock built-in mail() function.

--- tests/DBConnectionWatcherTest.php
+++ tests/DBConnectionWatcherTest.php
@@ -13,7 +13,7 @@ require_once(dirname(__FILE__) . '/../src/DBConnectionWatcher.php');
 use DBConnectionWatcher\DBConnectionWatcher;
 use DBConnectionWatcher\DB\DBMS\PostgreSQL;
 
-class DBConnectionWatcherTest extends PHPUnit_Framework_Testcase
+class DBConnectionWatcherTest extends \PHPUnit_Framework_Testcase
 {
     protected $dbConnectionWatcher;
 
@@ -24,7 +24,7 @@ class DBConnectionWatcherTest extends PHPUnit_Framework_Testcase
 
     protected function getMethod($name)
     {
-        $class = new ReflectionClass('DBConnectionWatcher\DBConnectionWatcher');
+        $class = new \ReflectionClass('DBConnectionWatcher\DBConnectionWatcher');
         $method = $class->getMethod($name);
         $method->setAccessible(true);
 
@@ -80,5 +80,138 @@ class DBConnectionWatcherTest extends PHPUnit_Framework_Testcase
         $method->invokeArgs($this->dbConnectionWatcher, [$db, $email, $connectionThreshold]);
 
         $this->closeConnections($connections);
+    }
+
+    public function testCheckStatusEqualThreshold()
+    {
+        $database = 'postgres';
+        $username = 'postgres';
+        $password = 'postgres';
+        $host     = 'localhost';
+        $port     = 5433;
+
+        $db = new PostgreSQL($database, $username, $password, $host, $port);
+        $email = 'julen.pardo@outlook.es';
+        $connectionThreshold = 5;
+
+        // The key part of the test is to assert that 'sendThresholdExceededMail' method is not called.
+        $mailer = $this->getMock('DBConnectionWatcher\Mailer\Mailer');
+        $mailer->expects($this->never())
+            ->method('sendThresholdExceededMail');
+
+        $this->dbConnectionWatcher->setMailer($mailer);
+
+        $connections = $this->createDatabaseConnections($database, $username, $password, $host, $port,
+            $connectionThreshold);
+
+        $method = $this->getMethod('checkStatus');
+        $method->invokeArgs($this->dbConnectionWatcher, [$db, $email, $connectionThreshold]);
+
+        $this->closeConnections($connections);
+    }
+
+    public function testCheckStatusAboveThreshold()
+    {
+        $database = 'postgres';
+        $username = 'postgres';
+        $password = 'postgres';
+        $host     = 'localhost';
+        $port     = 5433;
+
+        $db = new PostgreSQL($database, $username, $password, $host, $port);
+        $email = 'julen.pardo@outlook.es';
+        $connectionThreshold = 5;
+
+        // The key part of the test is to assert that 'sendThresholdExceededMail' method IS called.
+        $mailer = $this->getMock('DBConnectionWatcher\Mailer\Mailer');
+        $mailer->expects($this->once())
+            ->method('sendThresholdExceededMail');
+
+        $this->dbConnectionWatcher->setMailer($mailer);
+
+        $connections = $this->createDatabaseConnections($database, $username, $password, $host, $port,
+            $connectionThreshold + 1);
+
+        $method = $this->getMethod('checkStatus');
+        $method->invokeArgs($this->dbConnectionWatcher, [$db, $email, $connectionThreshold]);
+
+        $this->closeConnections($connections);
+    }
+
+    /**
+     * @expectedException \DBConnectionWatcher\DB\ConnectionException
+     */
+    public function testCheckStatusConnectionException()
+    {
+        $database = 'non_existing_database';
+        $username = 'postgres';
+        $password = 'postgres';
+        $host     = 'localhost';
+        $port     = 5433;
+
+        $db = new PostgreSQL($database, $username, $password, $host, $port);
+        $email = 'julen.pardo@outlook.es';
+        $connectionThreshold = 5;
+
+        $method = $this->getMethod('checkStatus');
+        $method->invokeArgs($this->dbConnectionWatcher, [$db, $email, $connectionThreshold]);
+    }
+
+    /**
+     * @expectedException \DBConnectionWatcher\Mailer\MailSendException
+     */
+    public function testCheckStatusMailSendException()
+    {
+        $database = 'postgres';
+        $username = 'postgres';
+        $password = 'postgres';
+        $host     = 'localhost';
+        $port     = 5433;
+
+        $db = new PostgreSQL($database, $username, $password, $host, $port);
+        $email = 'julen.pardo@outlook.es';
+        $connectionThreshold = 5;
+
+        $mailer = $this->getMock('DBConnectionWatcher\Mailer\Mailer');
+        $mailer->expects($this->once())
+               ->method('sendThresholdExceededMail')
+               ->will($this->throwException(new \DBConnectionWatcher\Mailer\MailSendException()));
+
+        $this->dbConnectionWatcher->setMailer($mailer);
+
+        $connections = $this->createDatabaseConnections($database, $username, $password, $host, $port,
+            $connectionThreshold + 1);
+
+        $method = $this->getMethod('checkStatus');
+        $method->invokeArgs($this->dbConnectionWatcher, [$db, $email, $connectionThreshold]);
+
+        $this->closeConnections($connections);
+        $db->disconnect();
+    }
+
+    public function testCheckStatus()
+    {
+        $this->markTestSkipped('This cannot be still tested because the mail() function cannot be mocked.');
+        $database = 'postgres';
+        $username = 'postgres';
+        $password = 'postgres';
+        $host     = 'localhost';
+        $port     = 5433;
+
+        $db = new PostgreSQL($database, $username, $password, $host, $port);
+        $email = 'julen.pardo@outlook.es';
+        $connectionThreshold = 5;
+
+        $connections = $this->createDatabaseConnections($database, $username, $password, $host, $port,
+            $connectionThreshold + 1);
+
+        $method = $this->getMethod('checkStatus');
+
+        try {
+            $method->invokeArgs($this->dbConnectionWatcher, [$db, $email, $connectionThreshold]);
+        } catch (\Exception $exception) {
+
+            $this->fail('No exception should be thrown: ' . $exception->getMessage());
+        }
     }
 }
