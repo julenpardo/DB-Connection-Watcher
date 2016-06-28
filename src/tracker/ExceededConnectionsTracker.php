286be18260763c2056449ec1dcec2018f4ae8fcd
Julen Pardo <julen.pardo@alumni.mondragon.edu>
2016-06-28 09:35:32 +0200

    dev-trackExceed: create class to read/write information of databases exceeding connections

---
+++ src/tracker/ExceededConnectionsTracker.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace DBConnectionWatcher\Tracker;
+
+class ExceededConnectionTracker
+{
+
+    public static function readAllDatabases($path)
+    {
+
+    }
+
+    public static function saveExceededDatabase($database, $host)
+    {
+
+    }
+
+    protected static function cleanDatabases($path)
+    {
+
+    }
+}
